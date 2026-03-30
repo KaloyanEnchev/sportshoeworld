c9b6853e9066edbbbe14be422cadea1f85ec134a
Kaloyan Enchev <k.enchev21@gmail.com>
2026-03-30 15:28:24 +0300

    Improve code structure

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-from django.core.validators import MinLengthValidator, RegexValidator
 from django.db import models
 
 from accounts.managers import AppUserManager
