812f0916219ee289f7e8871385b7f834970de31e
Kaloyan Enchev <k.enchev21@gmail.com>
2026-04-03 04:19:04 +0300

    Fixed urls

--- SportShoeWorld/urls.py
+++ SportShoeWorld/urls.py
@@ -28,6 +28,3 @@ urlpatterns = [
     path('account/', include('accounts.urls')),
     path('api/', include('api_shoes.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
